e42e1ac56a19be42959e73558e97d4c4cdc02440
HarmonySeeker <HarmonySeeker@users.noreply.github.com>
2018-09-29 16:37:41 +0700

    bankProjectNEWBETA.go is now bankProject.go, which was renewed with new option 'Retrieve' from database to XML, project is now almost ready, also updated 'README.me'

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ An XML to DB, DB to XML project, which was builded on GO.
 Main file is bankProject.go, there are some settings in this file, which affect everything else in the package.
 Right at the beginning of the main() function you will see "settings blog" with two settings:
 
-"ConnectionDB" is string for mysql connection handler, in this string you can choose your user, password, port and database to connect. 
+"ConnectionDB" is string for mysql connection handler, in this string you can choose your user, password, port and database to connect.
 
-"settings" string is responsible for switching modes: Import and Retrieve, but the whole functioanlity of this feature is now deactivated due to me not realising how to do a "Retrieve" part properly("I've made it really really close to how it should look in the end, but time's up). In this version only "Import" is available. Import takes data from "bank.Credentials.xml" and imports it in the database, checking and creating tables by his own. Retrieve should take data from database and convert it into the XML file.
+"settings" string is responsible for switching modes: Import and Retrieve. Import takes data from "bankCredentialsIN.xml" and imports it in the database, checking and creating tables by his own. Retrieve should take data from database and convert it into the XML file called "BankCredentialsOUT.xml".
 
 ----------------------
 Database creation
@@ -17,11 +17,11 @@ Once you have installed it, created user/password and chosen a port to start a m
 
 To operate with databases GoLang requires drivers, which are needed to be installed from: https://github.com/go-sql-driver/mysql
 
-WARNING: all .go files in the repository are one package, so they are needed to start simultaneously(e.g. from CMD the command will be "go run bankProject.go dbAccount.go dbCard.go dbClient.go" 
+WARNING: all .go files in the repository are one package, so they are needed to start simultaneously(e.g. from CMD the command will be "go run bankProject.go dbAccount.go dbCard.go dbClient.go"
 
-When the database is created, and the name of your database is similar to the name of database written in "ConnectionDB", all drivers are installed, you can start the script with the command which was mentiond earlier. 
+When the database is created, and the name of your database is similar to the name of database written in "ConnectionDB", all drivers are installed, you can start the script with the command which was mentiond earlier.
 
 The script will automatically connect to the database you have mentioned in the "ConnectionDB" string and check the tables which are need for its work. It will create all missing tables and fill them with data from "bankCredentials.xml"
 
 -------------
-Added bankProjectNOTREADY.go which demonstrates what I was working on with exporting data from database to XML, but it works not how it's must working. I am trying to fix it right now. 
+Added bankProjectNOTREADY.go which demonstrates what I was working on with exporting data from database to XML, but it works not how it's must working. I am trying to fix it right now.
--- bankProject.go
+++ bankProject.go
@@ -5,7 +5,10 @@ import (
   "fmt"
   "io/ioutil"
   "strings"
+  "database/sql"
   _ "github.com/go-sql-driver/mysql"
+  "os"
+  "io"
 )
 
 type clientCredentials struct{
@@ -50,68 +53,194 @@ type accountMap struct{
   Balance float64
 }
 
-var ConnectionDB string
-var settings string
+
+type clientFullToXML struct{
+  XMLName xml.Name `xml:"clients"`
+  Client []clientToXML `xml:"client"`
+}
+
+type clientToXML struct{
+  XMLName xml.Name `xml:"client"`
+
+  IndNum int `xml:"uniNumber"`
+  Name string `xml:"name"`
+  Surname string `xml:"surname"`
+  BirthDate string `xml:"dateOfBirth"`
+  Address string `xml:"address"`
+  Phone string `xml:"phoneNumber"`
+  Passport string `xml:"passportID"`
+  Cards Cards  `xml:"cards"`
+  Accounts Accounts `xml:"accounts"`
+}
+
+type Cards struct{
+    Card []Card `xml:"card"`
+}
+type Card struct{
+    CardID string `xml:"cardNumber"`
+    CardExpDate string `xml:"expirationDate"`
+    CardCurrency string `xml:"cardCurrency"`
+    CardBalance float64 `xml:"cardBalance"`
+}
+
+type Accounts struct{
+    Account []Account `xml:"account"`
+}
+type Account struct{
+    AccCurrency string `xml:"accountCurrency"`
+    AccBalance float64 `xml:" accountBalance"`
+}
+
+ var ConnectionDB string
+ var settings string
+
+func RetrieveClient(dt map[int]clientMap, db *sql.DB, Uni int) map[int]clientMap{
+  var innerArr clientMap
+  var Uniq int
+  var Name, Date, Address, Phone, Passport string
+  err := db.QueryRow("select * from clientsBio where Uni = ?", Uni).Scan(&Uniq, &Name, &Date, &Address, &Phone, &Passport)
+  checkErr(err)
+  innerArr = clientMap{Uniq, Name, Date, Address, Phone, Passport}
+  dt[0] = innerArr
+  return dt
+}
+
+func RetrieveCard(dt map[int]cardMap, db *sql.DB, Uni int) map[int]cardMap{
+  var ID, ExpDate, Currency string
+  var Balance float64
+  var CMap cardMap
+  var Uniq int
+  i := 0
+  rows, err := db.Query("select * from clientsCard where Uni = ?", Uni)
+  checkErr(err)
+  defer rows.Close()
+  for rows.Next() {
+    err := rows.Scan(&Uniq, &ID, &ExpDate, &Currency, &Balance)
+    checkErr(err)
+    CMap = cardMap{Uniq, ID, ExpDate, Currency, Balance}
+    dt[i] = CMap
+    i++
+  }
+  err = rows.Err()
+  checkErr(err)
+  return dt
+}
+
+func RetrieveAccount(dt map[int]accountMap, db *sql.DB, Uni int)map[int]accountMap{
+  var Uniq int
+  var Currency string
+  var Balance float64
+  var aMap accountMap
+  i := 0
+  rows, err := db.Query("select * from clientsAccount where Uni = ?", Uni)
+  checkErr(err)
+  defer rows.Close()
+  for rows.Next() {
+    err := rows.Scan(&Uniq, &Currency, &Balance)
+    checkErr(err)
+    aMap = accountMap{Uniq, Currency, Balance}
+    dt[i] = aMap
+    i++
+  }
+  err = rows.Err()
+  checkErr(err)
+  return dt
+}
 
 func main(){
   //settings blog; check README for more info
-  ConnectionDB = "user:passwd@tcp(127.0.0.1:3306)/bankProject" //change it for your data
-  settings = "Import"
-  //UniqueClient := 77602
+  ConnectionDB = "Wanderer:Art.156.DBW.426@tcp(127.0.0.1:3306)/bankProject"
+  settings = "Retrieve"
+  UniqueClient := 77602
   //end of settings blog
 
   clientInfo := make(map[int]clientMap)
   cardInfo := make(map[int]cardMap)
   accountInfo := make(map[int]accountMap)
 
-  var XMLdata clientCredentials
+  if settings == "Import"{
+    var XMLdata clientCredentials
 
-  fmt.Println("\nStarted:\n")
-  fmt.Println("Importing XML data...\n")
+    fmt.Println("\nStarted:\n")
+    fmt.Println("Importing XML data...\n")
 
-  XMLinput, _ := ioutil.ReadFile("bankCredentials.xml")
-  xml.Unmarshal(XMLinput, &XMLdata)
+    XMLinput, _ := ioutil.ReadFile("bankCredentials.xml")
+    xml.Unmarshal(XMLinput, &XMLdata)
 
-  for idx, _ := range XMLdata.ClientName{
-    clientUni := XMLdata.ClientUni[idx]
+    for idx, _ := range XMLdata.ClientName{
+      clientUni := XMLdata.ClientUni[idx]
 
-    for idx, _  := range XMLdata.ClientName{
-      clientFullName := strings.Join([]string{XMLdata.ClientName[idx], XMLdata.ClientSurname[idx]} , " ")
-      clientInfo[idx] = clientMap{clientUni, clientFullName, XMLdata.ClientBirth[idx], XMLdata.ClientAddress[idx], XMLdata.ClientPhone[idx], XMLdata.ClientPassport[idx]}
+      for idx, _  := range XMLdata.ClientName{
+        clientFullName := strings.Join([]string{XMLdata.ClientName[idx], XMLdata.ClientSurname[idx]} , " ")
+        clientInfo[idx] = clientMap{clientUni, clientFullName, XMLdata.ClientBirth[idx], XMLdata.ClientAddress[idx], XMLdata.ClientPhone[idx], XMLdata.ClientPassport[idx]}
+      }
+
+      for idx, _ := range XMLdata.CardID{
+        cardInfo[idx] = cardMap{clientUni, XMLdata.CardID[idx], XMLdata.CardExpDate[idx], XMLdata.CardCurrency[idx], XMLdata.CardBalance[idx]}
+      }
+
+      for idx, _ := range XMLdata.CardID{
+        accountInfo[idx] = accountMap{clientUni, XMLdata.AccountCurrency[idx], XMLdata.AccountBalance[idx]}
+      }
     }
-
-    for idx, _ := range XMLdata.CardID{
-      cardInfo[idx] = cardMap{clientUni, XMLdata.CardID[idx], XMLdata.CardExpDate[idx], XMLdata.CardCurrency[idx], XMLdata.CardBalance[idx]}
+    /*
+    fmt.Println("clientMap:")
+    for idx, data := range clientInfo{
+      fmt.Println(idx,":", data.IndNum, data.FullName, data.BirthDate, data.Address, data.Phone, data.Passport)
     }
+    fmt.Println()
 
-    for idx, _ := range XMLdata.CardID{
-      accountInfo[idx] = accountMap{clientUni, XMLdata.AccountCurrency[idx], XMLdata.AccountBalance[idx]}
+    fmt.Println("cardMap:")
+    for idx, data := range cardInfo{
+      fmt.Println(idx,":", data.IndNum, data.ID, data.ExpDate, data.Currency, data.Balance)
     }
-  }
-  /*
-  fmt.Println("clientMap:")
-  for idx, data := range clientInfo{
-    fmt.Println(idx,":", data.IndNum, data.FullName, data.BirthDate, data.Address, data.Phone, data.Passport)
-  }
-  fmt.Println()
+    fmt.Println()
 
-  fmt.Println("cardMap:")
-  for idx, data := range cardInfo{
-    fmt.Println(idx,":", data.IndNum, data.ID, data.ExpDate, data.Currency, data.Balance)
-  }
-  fmt.Println()
+    fmt.Println("accountMap:")
+    for idx, data := range accountInfo {
+      fmt.Println(idx,":", data.IndNum, data.Currency, data.Balance)
+    }
+    fmt.Println()*/
 
-  fmt.Println("accountMap:")
-  for idx, data := range accountInfo {
-    fmt.Println(idx,":", data.IndNum, data.Currency, data.Balance)
-  }
-  fmt.Println()*/
+    //Commented code above can help in debugging
 
-  //Commented code above can help in debugging
+    dbClientWriter(clientInfo)
+    dbCardWriter(cardInfo)
+    dbAccountWriter(accountInfo)
 
-  dbClientWriter(clientInfo)
-  dbCardWriter(cardInfo)
-  dbAccountWriter(accountInfo)
+    fmt.Println("Imported!")
+  } else if settings == "Retrieve"{
+      XMLOutput := clientToXML{}
+      db, err := sql.Open("mysql", ConnectionDB)
+      checkErr(err)
+      defer db.Close()
 
-  fmt.Println("Imported!")
+      RetrieveClient(clientInfo, db, UniqueClient)
+      RetrieveCard(cardInfo, db, UniqueClient)
+      RetrieveAccount(accountInfo, db, UniqueClient)
+
+      Name := strings.Split(string(clientInfo[0].FullName), " ")
+      XMLOutput = clientToXML{IndNum: clientInfo[0].IndNum, Name: Name[0], Surname: Name[1], BirthDate: clientInfo[0].BirthDate, Address: clientInfo[0].Address, Phone: clientInfo[0].Phone, Passport: clientInfo[0].Passport}
+
+      for _, data := range cardInfo{
+        XMLOutput.Cards.Card = append(XMLOutput.Cards.Card, Card{CardID: data.ID, CardExpDate: data.ExpDate, CardCurrency: data.Currency, CardBalance: data.Balance})
+      }
+
+      for _, data := range cardInfo{
+        XMLOutput.Accounts.Account = append(XMLOutput.Accounts.Account, Account{AccCurrency: data.Currency, AccBalance: data.Balance})
+      }
+
+
+      filename := "BankCredentialsOUT.xml"
+      file, _ := os.Create(filename)
+
+      xmlWriter := io.Writer(file)
+
+      enc := xml.NewEncoder(xmlWriter)
+      enc.Indent(" ","  ")
+      if err := enc.Encode(XMLOutput); err != nil {
+                 checkErr(err)
+         }
+
+  } else {fmt.Println("Error: Wrong settings parameter")}
 }
